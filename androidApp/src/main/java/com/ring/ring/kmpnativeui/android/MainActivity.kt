1079826624eeb2b3888f5f0aeaeda54d98af18a8
ringring <ringring.creator@gmail.com>
2024-03-11 10:40:31 +0900

    delete preview

--- androidApp/src/main/java/com/ring/ring/kmpnativeui/android/MainActivity.kt
+++ androidApp/src/main/java/com/ring/ring/kmpnativeui/android/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ring.ring.kmpnativeui.Greeting
@@ -64,13 +63,5 @@ private fun GreetingView(phrases: List<String>) {
             Text(phrase)
             Divider()
         }
-    }
-}
-
-@Preview
-@Composable
-private fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView(listOf("Hello", "Android!"))
     }
 }
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -45,9 +45,3 @@ extension ContentView {
         }
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
